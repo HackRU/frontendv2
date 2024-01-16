69f55b6e295f026a302d5f33d6afc7d2d6c3fc05
Kevin Monisit <monisitkevin@gmail.com>
2024-01-16 11:26:14 -0500

    new landing page template

---
+++ app/(landing)/sections/Hero.tsx
@@ -0,0 +1,15 @@
+export default function Hero() {
+  return (
+    <div
+      className="bg-gray-100 w-full h-[100vh] max-h-[1300px]
+      flex flex-col justify-center items-center"
+    >
+      <h1 className="font-extrabold text-5xl">
+        HackRU!
+      </h1>
+      <span>
+        Hack all knight looonnnggg
+      </span>
+    </div>
+  );
+}
