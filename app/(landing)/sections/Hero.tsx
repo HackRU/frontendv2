e255134f31a5f358d12c985c1ef0dadb54cddd87
Kevin Monisit <monisitkevin@gmail.com>
2024-01-19 11:05:00 -0500

    set up initial template

--- app/(landing)/sections/Hero.tsx
+++ app/(landing)/sections/Hero.tsx
@@ -1,15 +1,29 @@
+import Image from 'next/image';
+
 export default function Hero() {
   return (
     <div
       className="bg-gray-100 w-full h-[100vh] max-h-[1300px]
-      flex flex-col justify-center items-center"
+      flex flex-col-reverse justify-center items-center"
     >
-      <h1 className="font-extrabold text-5xl">
-        HackRU!
-      </h1>
-      <span>
-        Hack all knight looonnnggg
-      </span>
+      <Image
+        src="/landing/fire.png"
+        width="0"
+        height="0"
+        sizes="100vw"
+        alt="Fire"
+        /* https://stackoverflow.com/questions/69230343/nextjs-image-component-with-fixed-witdth-and-auto-height */
+        className="w-80 h-auto"
+        priority
+      />
+
+      <div className="flex flex-col text-center grow justify-center text-3xl space-y-4">
+        <div className="text-xl mb-2">WELCOME TO OUR</div>
+        <div>SCHOOL OF</div>
+        <div>CODECRAFT &</div>
+        <div>WIZARDRY!</div>
+      </div>
+
     </div>
   );
 }
--- prettier.config.js
+++ prettier.config.js
@@ -3,4 +3,7 @@ const styleguide = require('@vercel/style-guide/prettier');
 module.exports = {
   ...styleguide,
   plugins: [...styleguide.plugins, 'prettier-plugin-tailwindcss'],
+  rules: {
+    singleAttributePerLine: true,
+  },
 };
