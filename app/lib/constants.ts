6ee143ef54d00b720813b07d8620ace25b6b55b4
Kevin Monisit <monisitkevin@gmail.com>
2024-01-26 11:40:12 -0500

    About is added

---
+++ app/lib/constants.ts
@@ -0,0 +1,32 @@
+export const hackRUFAQ = {
+  whatIsHackRUAnswer: `HackRU is a 24-hour hackathon at Rutgers University. We welcome hundreds of students to join us in building
+  awesome software and hardware projects. Industry experts and mentors come from all over the country to create
+  an environment that fosters an atmosphere of learning through teck talks and one-on-one guidance.
+  We encourage beginner and advanced hackers alike to challenge themselves and expand their skills.`,
+
+  whatIsApplicationAnswer: `HackRU will be back in person this Fall 2023! We will be accepting anywhere between 300-500 hackers based on
+  when you register for the event. After you register, you'll get a notification 1-2 weeks before the hackathon whether we have accepted you or
+  not to the hackathon. You will then have to let us know if you plan on coming or not and then you're all set!`,
+
+  winAnythingAnswer:
+    "Yes! We'll release more information about prizes as the event draws near.",
+
+  maskMandateAnswer:
+    'No, HackRU will not have a mask mandate but it is encouraged throughout the duration of the event.',
+
+  moreQuestionsAnswer:
+    "Reach out to us at info@hackru.org! We'll be happy to answer.",
+
+  whoCanComeAnswer: `HackRU welcomes undergraduate and graduate students of all majors, backgrounds, and skill level to come create. Additionally, high
+  school students who will be 18 by HackRU are allowed to register. Unfortunately, if you are under 18, you will not be able to attend.`,
+
+  newAnswer: `That's great! We'd love to have you at HackRU. Throughout the weekend, there will be workshops to get your feet wet,
+  project ideas for you to try out, and mentors to guide you through the process.`,
+
+  costAnswer:
+    'Like the best things in life, HackRU is completely free to attend!',
+
+  workWithOthersAnswer: `Hackers can(and are encouraged to) work in teams of up to 4 humans max. The knowledge you gain from teammates is
+  invaluable, along with the opportunity to build lifetime friendships - you might leave with a new best friend! We will be having a team-building exercise
+  after opening ceremonies for people who are looking for teammates.`,
+};
