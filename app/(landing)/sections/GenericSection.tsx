dfa03687b66f1dbcd4903448f24d44a83133ed65
bhivam <shivamkajaria@gmail.com>
2024-01-27 19:22:32 -0500

    added some colors and additional divider

--- app/(landing)/sections/GenericSection.tsx
+++ app/(landing)/sections/GenericSection.tsx
@@ -11,10 +11,11 @@ export default function GenericSection(props: GenericSectionProps) {
   return (
     <div
       id={props.title}
-      className={`flex w-full flex-col items-center bg-${props.color}`}
+      className={'flex w-full flex-col items-center bg-blue-200'}
     >
       <SectionTitle title={props.title} />
       {props.children}
+      <div className='h-[15vh] w-full bg-blue-100'></div>
     </div>
   );
 }
--- app/(landing)/sections/SectionTitle.tsx
+++ app/(landing)/sections/SectionTitle.tsx
@@ -3,17 +3,17 @@ export default function SectionTitle(props: { title: string }) {
     <div className="flex w-full items-center">
       <div className="w-[7.5vw]" />
       <div className="flex w-1/4 items-center">
-        <div className="h-[0px] flex-1 border-4 border-dashed border-black" />
+        <div className="h-[0px] flex-1 border-4 border-dashed border-orange-100" />
         <div className="w-[5px]" />
-        <div className="h-[17px] w-[17px] rotate-45 bg-black" />
+        <div className="h-[17px] w-[17px] rotate-45 bg-orange-100" />
       </div>
-      <div className="text-black-500 justify-content flex-grow text-center text-8xl">
-        {props.title}
+      <div className="text-orange-100 justify-content flex-grow text-center text-8xl">
+        {props.title} 
       </div>
       <div className="flex w-1/4 items-center">
-        <div className="h-[17px] w-[17px] rotate-45 bg-black" />
+        <div className="h-[17px] w-[17px] rotate-45 bg-orange-100" />
         <div className="w-[5px]" />
-        <div className="h-[0px] flex-1 border-4 border-dashed border-black" />
+        <div className="h-[0px] flex-1 border-4 border-dashed border-orange-100" />
       </div>
       <div className="w-[7.5vw]" />
     </div>
