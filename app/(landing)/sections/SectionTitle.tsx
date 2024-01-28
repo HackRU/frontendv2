bc4565bb795af1697bc86582d6286f11bf031c40
bhivam <shivamkajaria@gmail.com>
2024-01-27 19:25:17 -0500

    moved text divider half way up to previous section

--- app/(landing)/sections/SectionTitle.tsx
+++ app/(landing)/sections/SectionTitle.tsx
@@ -1,6 +1,6 @@
 export default function SectionTitle(props: { title: string }) {
   return (
-    <div className="flex w-full items-center">
+    <div className="flex w-full items-center transform -translate-y-1/2">
       <div className="w-[7.5vw]" />
       <div className="flex w-1/4 items-center">
         <div className="h-[0px] flex-1 border-4 border-dashed border-orange-100" />
