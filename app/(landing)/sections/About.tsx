41aed28a82986921c830a28aaec3fad5a15b419d
Kevin Monisit <monisitkevin@gmail.com>
2024-01-30 21:42:03 -0500

    Merging from Dev (#35)
    * Hero Section (#27)
    
    * set up initial template
    
    * added navbar to hero section
    
    * responsive hero section
    
    * Basic About is done
    
    * About is added
    
    * created cursor component with wand image (#30)
    
    * "created cursor component with wang image
     with '#' will be ignored, and an empty message aborts the commit.
    
    * Created Simple About Section (#31)
    
    * Hero Section (#27)
    
    * set up initial template
    
    * added navbar to hero section
    
    * responsive hero section
    
    * Basic About is done
    
    * Merge from Dev (#33)
    
    * Hero Section (#27)
    
    * set up initial template
    
    * added navbar to hero section
    
    * responsive hero section
    
    * Basic About is done
    
    * About is added
    
    * fixed cursor selection bugs and added a trail
    
    * Fixed missing component import for FAQ
    
    * added comment in luge.js
    
    ---------
    
    Co-authored-by: Kevin Monisit <monisitkevin@gmail.com>
    
    * Basic Sections (#32)
    
    * completed basic generic sections
    
    * Formatting with prettier
    
    * added some colors and additional divider
    
    * moved text divider half way up to previous section
    
    ---------
    
    Co-authored-by: Kevin Monisit <monisitkevin@gmail.com>
    
    ---------
    
    Co-authored-by: poojakedia <67982568+poojakedia@users.noreply.github.com>
    Co-authored-by: Shivam Kajaria <57373405+bhivam@users.noreply.github.com>

--- app/(landing)/sections/About.tsx
+++ app/(landing)/sections/About.tsx
@@ -1,29 +1,30 @@
 import Image from 'next/image';
 import clsx from 'clsx';
 
-function AboutInfo({ children, title, imageSrc, alt, reverse }:
-  {
-    children: React.ReactNode,
-    title: string,
-    imageSrc: string,
-    alt: string,
-    reverse?: boolean,
-  }
-) {
-
+function AboutInfo({
+  children,
+  title,
+  imageSrc,
+  alt,
+  reverse,
+}: {
+  children: React.ReactNode;
+  title: string;
+  imageSrc: string;
+  alt: string;
+  reverse?: boolean;
+}) {
   function AboutInfoContent() {
     return (
       <div
         className={clsx(
-          "bg-red-100 w-full md:w-1/2 md:grow md:justify-end h-fit p-20",
+          'h-fit w-full bg-red-100 p-20 md:w-1/2 md:grow md:justify-end',
           {
             'text-end': reverse,
-          }
+          },
         )}
       >
-        <h1 className="font-extrabold bg-red-100 text-5xl">
-          {title}
-        </h1>
+        <h1 className="bg-red-100 text-5xl font-extrabold">{title}</h1>
         {children}
       </div>
     );
@@ -33,20 +34,14 @@ function AboutInfo({ children, title, imageSrc, alt, reverse }:
     return (
       <div
         className={clsx(
-          "bg-red-200 w-full md:w-1/2 h-fit flex justify-center",
+          'flex h-fit w-full justify-center bg-red-200 md:w-1/2',
           {
-            "md:justify-start": !reverse,
-            "md:justify-end": reverse,
-          }
+            'md:justify-start': !reverse,
+            'md:justify-end': reverse,
+          },
         )}
       >
-        <Image
-          src={imageSrc}
-          width="400"
-          height="400"
-          alt={alt}
-          priority
-        />
+        <Image src={imageSrc} width="400" height="400" alt={alt} priority />
       </div>
     );
   }
@@ -57,7 +52,7 @@ function AboutInfo({ children, title, imageSrc, alt, reverse }:
         <AboutImage />
         <AboutInfoContent />
       </>
-    )
+    );
   }
 
   return (
@@ -65,49 +60,51 @@ function AboutInfo({ children, title, imageSrc, alt, reverse }:
       <AboutInfoContent />
       <AboutImage />
     </>
-  )
+  );
 }
-
 
 export default function About() {
   return (
     <div
-      className="bg-gray-200 w-full h-fit
-      flex flex-col md:flex-row flex-wrap"
+      className="flex h-fit w-full
+      flex-col flex-wrap bg-gray-200 md:flex-row"
     >
-      <AboutInfo title="WHAT" imageSrc='/landing/python.png' alt="Python">
+      <AboutInfo title="WHAT" imageSrc="/landing/python.png" alt="Python">
         <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-          imperdiet, nibh nec dictum consectetur, lorem nisi
+          imperdiet, nibh nec dictum consectetur, lorem nisi Lorem ipsum dolor
+          sit amet, consectetur adipiscing elit. Sed imperdiet, nibh nec dictum
+          consectetur, lorem nisi Lorem ipsum dolor sit amet, consectetur
+          adipiscing elit. Sed imperdiet, nibh nec dictum consectetur, lorem
+          nisi
+        </p>
+      </AboutInfo>
+
+      <AboutInfo
+        title="TRACKS"
+        imageSrc="/landing/python.png"
+        alt="Python"
+        reverse
+      >
+        <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-          imperdiet, nibh nec dictum consectetur, lorem nisi
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+          imperdiet, nibh nec dictum consectetur, lorem nisi Lorem ipsum dolor
+          sit amet, consectetur adipiscing elit. Sed imperdiet, nibh nec dictum
+          consectetur, lorem nisi Lorem ipsum dolor sit amet, consectetur
+          adipiscing elit. Sed imperdiet, nibh nec dictum consectetur, lorem
+          nisi Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
           imperdiet, nibh nec dictum consectetur, lorem nisi
         </p>
       </AboutInfo>
 
-      <AboutInfo title="TRACKS" imageSrc='/landing/python.png' alt="Python" reverse>
+      <AboutInfo title="JOIN US" imageSrc="/landing/python.png" alt="Python">
         <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-          imperdiet, nibh nec dictum consectetur, lorem nisi
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-          imperdiet, nibh nec dictum consectetur, lorem nisi
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-          imperdiet, nibh nec dictum consectetur, lorem nisi
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-          imperdiet, nibh nec dictum consectetur, lorem nisi
-        </p>
-      </AboutInfo>
-
-      <AboutInfo title="JOIN US" imageSrc='/landing/python.png' alt="Python">
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-          imperdiet, nibh nec dictum consectetur, lorem nisi
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-          imperdiet, nibh nec dictum consectetur, lorem nisi
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-          imperdiet, nibh nec dictum consectetur, lorem nisi
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+          imperdiet, nibh nec dictum consectetur, lorem nisi Lorem ipsum dolor
+          sit amet, consectetur adipiscing elit. Sed imperdiet, nibh nec dictum
+          consectetur, lorem nisi Lorem ipsum dolor sit amet, consectetur
+          adipiscing elit. Sed imperdiet, nibh nec dictum consectetur, lorem
+          nisi Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
           imperdiet, nibh nec dictum consectetur, lorem nisi
         </p>
       </AboutInfo>
--- app/(landing)/sections/Hero/Hero.tsx
+++ app/(landing)/sections/Hero/Hero.tsx
@@ -8,21 +8,21 @@ export default function Hero() {
       <div
         className="flex w-full
         flex-col items-center justify-center bg-gray-100
-        md:flex md:flex-row-reverse md:h-[100vh]
+        md:flex md:h-[100vh] md:flex-row-reverse
         "
       >
-
-
         {/* <div className="w-full h-[75vh] bg-red-100" />
         <div className="w-full h-10 bg-red-500" /> */}
 
-        <div className="w-full lg:w-2/5 h-[40vh] text-center pt-10 md:pt-0 relative">
+        <div className="relative h-[40vh] w-full pt-10 text-center md:pt-0 lg:w-2/5">
           <div
             className="flex h-[40vh] w-full flex-col justify-center space-y-4 text-5xl
-                       md:text-4xl md:space-y-7 md:absolute md:-left-20 md:min-w-fit
-                       lg:text-5xl lg:-left-10 xl:text-6xl xl:space-y-8"
+                       md:absolute md:-left-20 md:min-w-fit md:space-y-7 md:text-4xl
+                       lg:-left-10 lg:text-5xl xl:space-y-8 xl:text-6xl"
           >
-            <div className="mb-2 text-xl md:mb-0 lg:text-3xl xl:text-4xl">WELCOME TO OUR</div>
+            <div className="mb-2 text-xl md:mb-0 lg:text-3xl xl:text-4xl">
+              WELCOME TO OUR
+            </div>
             <div>SCHOOL OF</div>
             <div>CODECRAFT&nbsp;&</div>
             <div>CIRCUITRY!</div>
