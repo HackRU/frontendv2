7bfced0da73f7302db0a0ca8294b8af386f53e39
Kevin Monisit <monisitkevin@gmail.com>
2024-01-30 20:45:12 -0500

    added comment in luge.js

--- app/ui/luge.js
+++ app/ui/luge.js
@@ -3,6 +3,15 @@ import { useEffect } from 'react';
 
 import '@/node_modules/@waaark/luge/dist/css/luge.css';
 
+/**
+ * This is for the trailing effect for the wand in the Cursor component.
+ *
+ * Delete this when necessary. This adds a whole another library to this project
+ * and we don't need the clutter later on if we don't need it.
+ *
+ * But magic wand trail effect is very cool. So we keep for Spring '24.
+ */
+
 const LugeReact = () => {
   useEffect(() => {
     import('@waaark/luge/dist/js/luge')
