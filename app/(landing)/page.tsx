926947f1cc071610a8ec7af8284e352537ab782a
Kevin Monisit <monisitkevin@gmail.com>
2024-01-30 20:43:31 -0500

    Fixed missing component import for FAQ

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -8,7 +8,7 @@ import Schedule from './sections/Schedule';
 import { Suspense } from 'react';
 import Sponsors from './sections/Sponsors';
 import About from './sections/About';
-import FAQ from './sections/FAQ';
+import FAQ from './sections/FAQ/FAQ';
 import Cursor from '@/app/ui/cursor';
 
 export default function Page() {
