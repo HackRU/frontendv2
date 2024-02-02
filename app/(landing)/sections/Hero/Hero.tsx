86620b94f7ba99a88059e66b607650521c8fad05
dchaudhari0811 <dchaudhari2003@gmail.com>
2024-02-02 11:26:38 -0500

    Animating fire-animation

--- app/(landing)/sections/Hero/Hero.tsx
+++ app/(landing)/sections/Hero/Hero.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 import Navbar from './Navbar';
 import React, { useState, useEffect } from "react";
 
-const FIRE = [
+const FIRE_IMG = [
   "/landing/fire1.webp",
   "/landing/fire2.webp",
   "/landing/fire3.webp",
@@ -15,7 +15,7 @@ export default function Hero() {
 
   useEffect(() => {
     const intervalId = setInterval(() => {
-      setCurrentImageIndex((prevIndex) => (prevIndex + 1) % FIRE.length);
+      setCurrentImageIndex((prevIndex) => (prevIndex + 1) % FIRE_IMG.length);
     }, 218);
 
     return () => clearInterval(intervalId);
@@ -49,7 +49,7 @@ export default function Hero() {
         </div>
 
         <Image
-          src= {FIRE[currentImageIndex]}
+          src= {FIRE_IMG[currentImageIndex]}
           width="0"
           height="0"
           sizes="100vw"
