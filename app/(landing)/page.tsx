23fc5ce49f36d71e170f561ed6a7f4c38fe528e1
Shivam Kajaria <57373405+bhivam@users.noreply.github.com>
2024-02-04 13:24:29 -0500

    New FAQ (#47)
    * Accordian Style FAQ Unstyled
    Finished up accordian style FAQ without any styling applied
    yet. Changed the FAQ constants to include the question as well
    as answer.
    
    * Styled FAQ and Made Responsive
    Created responsive FAQ using breakpoints and styled accordian.
    
    * Fixed Typo
    Accordian -> Accordion

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,15 +1,10 @@
-import AcmeLogo from "@/app/ui/acme-logo";
-import { ArrowRightIcon } from "@heroicons/react/24/outline";
-import Link from "next/link";
-import { lusitana } from "@/app/ui/fonts";
-import Image from "next/image";
-import Hero from "./sections/Hero/Hero";
-import Schedule from "./sections/Schedule";
-import { Suspense } from "react";
-import Sponsors from "./sections/Sponsors";
-import About from "./sections/About";
-import FAQ from "./sections/FAQ/FAQ";
-import GenericSection from "./sections/GenericSection";
+import Hero from './sections/Hero/Hero';
+import Schedule from './sections/Schedule';
+import { Suspense } from 'react';
+import Sponsors from './sections/Sponsors';
+import About from './sections/About';
+import FAQ from './sections/FAQ/FAQ';
+import GenericSection from './sections/GenericSection';
 import Cursor from '@/app/ui/cursor';
 
 export default function Page() {
@@ -21,13 +16,11 @@ export default function Page() {
       <div>
         <Hero />
         <About />
-        {
-          /*
-           * We are using Suspense because Schedule and Sponsors will eventually
-           * pull from the backend. Also, we will need to replace the fallback
-           * component to a relevant loading component.
-           */
-        }
+        {/*
+         * We are using Suspense because Schedule and Sponsors will eventually
+         * pull from the backend. Also, we will need to replace the fallback
+         * component to a relevant loading component.
+         */}
         <GenericSection title="Schedule" color="white">
           <Suspense fallback={<>Loading Schedule!</>}>
             <Schedule />
---
+++ app/(landing)/sections/FAQ/Accordion.tsx
@@ -0,0 +1,56 @@
+import { useState } from 'react';
+
+type propType = {
+  question: string;
+  answer: string;
+};
+
+export default function Accordion(props: propType) {
+  const [accordionOpen, setAccordionOpen] = useState(false);
+
+  return (
+    <div>
+      <button
+        onClick={() => setAccordionOpen(!accordionOpen)}
+        className="flex w-full justify-between items-center pt-5"
+      >
+        <span className="text-3xl text-orange-100 text-left font-bold">{props.question}</span>
+        <svg
+          className="ml-8 shrink-0 fill-orange-100"
+          width="16"
+          height="16"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <rect
+            y="7"
+            width="16"
+            height="2"
+            rx="1"
+            className={`origin-center transform transition duration-200 ease-out ${
+              accordionOpen && '!rotate-180'
+            }`}
+          />
+          <rect
+            y="7"
+            width="16"
+            height="2"
+            rx="1"
+            className={`origin-center rotate-90 transform transition duration-200 ease-out ${
+              accordionOpen && '!rotate-180'
+            }`}
+          />
+        </svg>
+      </button>
+      <div
+        className={`pb-3 grid overflow-hidden text-sm text-orange-100 transition-all duration-300 ease-in-out ${
+          accordionOpen
+            ? 'grid-rows-[1fr] opacity-100'
+            : 'grid-rows-[0fr] opacity-0'
+        }`}
+      >
+        <div className="overflow-hidden text-left text-lg">{props.answer}</div>
+      </div>
+      <hr />
+    </div>
+  );
+}
