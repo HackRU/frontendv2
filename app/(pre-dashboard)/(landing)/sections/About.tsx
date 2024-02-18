1ec8eaa0ff8cc43d41fb0504178c5f253057767f
dchaudhari0811 <dchaudhari2003@gmail.com>
2024-02-18 10:53:38 -0500

    About Text format change

--- app/(pre-dashboard)/(landing)/sections/About.tsx
+++ app/(pre-dashboard)/(landing)/sections/About.tsx
@@ -89,17 +89,34 @@ export default function About() {
         className="flex h-fit w-full
         flex-col flex-wrap
         from-dark_blue_figma from-5% to-[#213537] bg-gradient-to-b
-        md:flex-row"
+        md:flex-row text-base md:text-lg xl:text-xl 2xl:text-2xl "
       >
-        <AboutInfo title="WHAT" imageSrc="/landing/roar.js.png" alt="Python">
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-            imperdiet, nibh nec dictum consectetur, lorem nisi Lorem ipsum dolor
-            sit amet, consectetur adipiscing elit. Sed imperdiet, nibh nec dictum
-            consectetur, lorem nisi Lorem ipsum dolor sit amet, consectetur
-            adipiscing elit. Sed imperdiet, nibh nec dictum consectetur, lorem
-            nisi
+
+        <AboutInfo title="WHAT"  imageSrc="/landing/roar.js.png" alt="Python">
+          <div className = 'pt-6'>
+          <p className = 'inline'>
+          HackRU is a
           </p>
+          <p className = 'inline font-bold'> 24-hour hackathon</p>
+          <p className = 'inline'>at Rutgers University. We welcome</p> 
+          <p className = 'inline font-bold'> hundreds of students</p>
+          <p className = 'inline'>to join us in building</p>
+          <p className = 'inline font-bold'> awesome tech projects.</p>
+          <p className = 'inline font-bold'> Industry experts</p>
+          <p className = 'inline'> and</p>
+          <p className = 'inline font-bold'> mentors</p>
+          <p className = 'inline'> help foster an atmosphere of</p>
+          <p className = 'inline font-bold'> learning</p>
+          <p className = 'inline'> through</p>
+          <p className = 'inline font-bold'> tech-talks</p>
+          <p className = 'inline'> and</p>
+          <p className = 'inline font-bold'> one-on-one guidance. We encourage</p>
+          <p className = 'inline font-bold'> all students,</p>
+          <p className = 'inline'> no matter their experience level or educational background, to</p> 
+          <p className = 'inline font-bold'> challenge themselves</p>
+          <p className = 'inline'> and expand their creative, technical, and collaboration skills</p> 
+          <p className = 'inline font-bold'> at HackRU.</p>
+          </div>
         </AboutInfo>
 
         <AboutInfo
@@ -108,27 +125,63 @@ export default function About() {
           alt="Python"
           reverse
         >
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-            imperdiet, nibh nec dictum consectetur, lorem nisi Lorem ipsum dolor
-            sit amet, consectetur adipiscing elit. Sed imperdiet, nibh nec dictum
-            consectetur, lorem nisi Lorem ipsum dolor sit amet, consectetur
-            adipiscing elit. Sed imperdiet, nibh nec dictum consectetur, lorem
-            nisi Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-            imperdiet, nibh nec dictum consectetur, lorem nisi
-          </p>
+          <div className = 'pt-6'>
+
+          <p className = 'inline font-bold'>Social Good:</p>
+          <p className = 'pt-4 pb-4 inline'> Hacks that betters the community.</p>
+
+          <div className = 'pt-6'>
+          <p className = 'inline font-bold'>Health:</p>
+          <p className = 'pb-4 inline'> Hacks that improving the mind or body, aiding with health, wellness, and fitness.</p>
+          </div>
+          
+          <div className = 'pt-6'>
+          <p className = 'inline font-bold'>Education:</p>
+          <p className = 'pb-4 inline'> Hacks that improving the mind or body, aiding with health, wellness, and fitness.</p>
+          </div>
+
+          <div className = 'pt-6'>
+          <p className = 'inline font-bold'>Maverick:</p>
+          <p className = 'pb-4 inline'> Any other hack! The</p>
+          <p>opportunities are limitless.</p>
+          </div>
+
+          <div className = 'pt-6'>
+          <p className = 'inline'>And more</p>
+          <p className = 'inline font-bold'> sponsor prizes!</p>
+          </div>
+
+          </div>
         </AboutInfo>
 
         <AboutInfo title="JOIN US" imageSrc="/landing/pseudoclaw.png" alt="Python">
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-            imperdiet, nibh nec dictum consectetur, lorem nisi Lorem ipsum dolor
-            sit amet, consectetur adipiscing elit. Sed imperdiet, nibh nec dictum
-            consectetur, lorem nisi Lorem ipsum dolor sit amet, consectetur
-            adipiscing elit. Sed imperdiet, nibh nec dictum consectetur, lorem
-            nisi Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-            imperdiet, nibh nec dictum consectetur, lorem nisi
+          <div className = 'pt-6 pb-6'>
+          <p className = 'inline font-bold'>
+          Apply
           </p>
+          <p className = 'inline'> to attend our</p>
+          <p className = 'inline font-bold'> Fall 2023 HackRU!</p> 
+          <p className = 'inline'> The event will be on</p> 
+          <p className = 'inline font-bold'> October 7th-8th</p> 
+          <p className = 'inline'> at the</p> 
+          <p className = 'inline font-bold'> College Avenue Student Center.</p>
+          </div>
+
+          <div className = 'pb-6'>
+
+          <p className = 'inline'>Want to help? Sign up to</p> 
+          <p className = 'inline font-bold'> volunteer</p> 
+          <p className = 'inline'> and/or</p> 
+          <p className = 'inline font-bold'> mentor!</p> 
+          <p className = 'inline'> To know when organizer applications open subscribe to our newsletter!</p>
+
+          </div>
+          <div className = 'pb-6' >
+          <p className = 'inline'>
+          Want to receive updates?
+          </p>
+          <p className = 'inline font-bold'> Subscribe here!</p>
+          </div>
         </AboutInfo>
       </div>
     </>
