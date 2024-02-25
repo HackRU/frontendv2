8222c8c7ecb7db65f43a1e1f6b536d8f0fafb8af
Kevin Monisit <monisitkevin@gmail.com>
2024-02-24 19:42:11 -0500

    Added favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,21 +4,20 @@ import { Metadata } from 'next';
 
 
 export const metadata: Metadata = {
-  title:{
+  title: {
     template: '%s | HackRU S24',
     default: 'HackRU S24',
   },
   description: "HackRU is a 24-hour hackathon hosted at Rutgers University. Join us for HackRU's Spring 2024 edition!",
-  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+  metadataBase: new URL('https://hackru.org'),
   icons: {
-    icon: '/icon.png', 
+    icon: '/icon.png',
   },
-  keywords:"Hackathon, HackRU, Rutgers, Technology, Programming, Innovation, Challenge",
-
+  keywords: "Hackathon, HackRU, Rutgers, Technology, Programming, Innovation, Challenge",
   //openGraph to set metadata for social media sharing
-  openGraph:{
-    title:  'HackRU S24',
-    description: "HackRU is a 24-hour hackathonat Rutgers University. We welcome hundreds of studentsto join us in building awesome tech projects. Industry experts and mentors help foster an atmosphere of learning through tech-talks and one-on-one guidance. We encourage all students, no matter their experience level or educational background, to challenge themselves and expand their creative, technical, and collaboration skills at HackRU",
+  openGraph: {
+    title: 'HackRU S24',
+    description: "HackRU is a 24-hour hackathon Rutgers University. We welcome hundreds of students to join us in building awesome tech projects. Industry experts and mentors help foster an atmosphere of learning through tech-talks and one-on-one guidance. We encourage all students, no matter their experience level or educational background, to challenge themselves and expand their creative, technical, and collaboration skills at HackRU",
   }
 };
 export default function RootLayout({
