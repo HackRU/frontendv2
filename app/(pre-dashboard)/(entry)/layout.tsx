7bb4c10210a2fc706fe192bb9bb5252f4d8e670c
Kevin Monisit <monisitkevin@gmail.com>
2024-03-18 14:27:32 -0400

    Message display for entry issues

--- app/(pre-dashboard)/(entry)/layout.tsx
+++ app/(pre-dashboard)/(entry)/layout.tsx
@@ -12,6 +12,15 @@ export default function Layout({ children
           id="entry-radial"
           className="top-0 w-full h-full absolute -z-10"
         />
+
+        <div
+          id="dialog"
+          className="fixed top-0 left-0 w-full h-full flex justify-center items-center bg-black bg-opacity-60 z-50"
+        >
+          <div className="bg-white p-4 rounded-lg">
+            <p>HackRU is currently experiencing issues with login and registration. Please come back later!</p>
+          </div>
+        </div>
       </main>
     </>
   )
