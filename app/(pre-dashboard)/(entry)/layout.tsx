19fd903fb9dae79ea4528617f21709068e1e4652
bhivam <shivamkajaria@gmail.com>
2024-03-19 10:28:46 -0400

    remove warning message

--- app/(pre-dashboard)/(entry)/layout.tsx
+++ app/(pre-dashboard)/(entry)/layout.tsx
@@ -1,27 +1,12 @@
-import { Suspense } from "react";
+import { Suspense } from 'react';
 
-export default function Layout({ children
-}: {
-  children: React.ReactNode;
-}) {
+export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <main className="w-fit h-fit relative">
+      <main className="relative h-fit w-fit">
         {children}
-        <div
-          id="entry-radial"
-          className="top-0 w-full h-full absolute -z-10"
-        />
-
-        <div
-          id="dialog"
-          className="fixed top-0 left-0 w-full h-full flex justify-center items-center bg-black bg-opacity-60 z-50"
-        >
-          <div className="bg-white p-4 rounded-lg">
-            <p>HackRU is currently experiencing issues with login and registration. Please come back later!</p>
-          </div>
-        </div>
+        <div id="entry-radial" className="absolute top-0 -z-10 h-full w-full" />
       </main>
     </>
-  )
+  );
 }
