f7cd4a6c2d356c04ab4c339aee1e50c409493f2a
avsomers25 <67980093+avsomers25@users.noreply.github.com>
2024-06-09 13:17:01 -0400

    Update definitions.ts

--- app/lib/definitions.ts
+++ app/lib/definitions.ts
@@ -1,4 +1,5 @@
-export const BASE = "https://api.hackru.org/dev";
+export const BASE = process.env.BASE_URL;
+//export const BASE = "https://api.hackru.org/dev"
 
 // This file contains type definitions for your data.
 // It describes the shape of the data, and what data type each property should accept.
