feb9fff77b0256dac793c4444b31bb40eece904e
Yogesh <yogeshsamkumar@gmail.com>
2024-06-13 12:18:57 -0500

    fixed config

--- auth.config.ts
+++ auth.config.ts
@@ -17,7 +17,7 @@ export const authConfig = {
       const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
       if (isOnDashboard) {
         if (isLoggedIn) return true;
-        return true; // Redirect unauthenticated users to login page
+        return false; // Redirect unauthenticated users to login page
       } 
       const isOnLogin = nextUrl.pathname.startsWith('/login');
       if (isOnLogin && isLoggedIn){
