1fdaf35087c320b1c927ac8fc2f15712e25dbca2
RyamL1221 <ryaml1221@gmail.com>
2024-07-11 16:52:30 -0400

    Added component for team members

--- app/(pre-dashboard)/(landing)/sections/Team.tsx
+++ app/(pre-dashboard)/(landing)/sections/Team.tsx
@@ -1,7 +1,28 @@
 import React from 'react';
 
-const Team = () => {
-  return <div>Hello</div>;
-};
+export default function Team() {
 
-export default Team;
+  const name = ["Team Member", "Team Member", "Team Member", "Team Member"];
+  const description = ["A short description of the team member", "A short description of the team member", "A short description of the team member",  "A short description of the team member"];
+  const image = ["https://wallpapers-clan.com/wp-content/uploads/2022/06/one-piece-monkey-d-luffy-pfp-3.jpg", "https://wallpapers-clan.com/wp-content/uploads/2022/06/one-piece-monkey-d-luffy-pfp-3.jpg", "https://wallpapers-clan.com/wp-content/uploads/2022/06/one-piece-monkey-d-luffy-pfp-3.jpg", "https://wallpapers-clan.com/wp-content/uploads/2022/06/one-piece-monkey-d-luffy-pfp-3.jpg"];
+
+  return (
+    <div className="relative z-10 mb-20 flex w-[100vw] justify-center"> 
+      <div className="flex flex-wrap justify-center">
+        {name.map((name, index) => {
+          return (
+            <div key={index} className="w-full md:w-1/3 p-4 flex flex-col items-center">
+              <div className="">
+                <img src={image[index]} alt="Team Member" className="rounded-3xl" />
+              </div>
+              <div style={{ zIndex: 1 }} className="text-center">
+                <h3 className="text-2xl font-bold">{name}</h3>
+                <p>{description[index]}</p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
