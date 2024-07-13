a509c5063d6050634c10db22549b1d88e9979e31
RyamL1221 <ryaml1221@gmail.com>
2024-07-12 21:39:27 -0400

    edited team component to use the team-roster component

--- app/(pre-dashboard)/(landing)/sections/Team.tsx
+++ app/(pre-dashboard)/(landing)/sections/Team.tsx
@@ -1,28 +1,20 @@
+import TeamRoster from '@/app/ui/team-roster';
 import React from 'react';
 
 export default function Team() {
+  const names = ["Team Member 1", "Team Member 2", "Team Member 3", "Team Member 4"];
+  const descriptions = [
+    "A short description of the team member. 1",
+    "A short description of the team member. 2",
+    "A short description of the team member. 3",
+    "A short description of the team member. 4"
+  ];
+  const images = [
+    "https://wallpapers-clan.com/wp-content/uploads/2022/06/one-piece-monkey-d-luffy-pfp-3.jpg",
+    "https://wallpapers-clan.com/wp-content/uploads/2022/06/one-piece-monkey-d-luffy-pfp-3.jpg",
+    "https://wallpapers-clan.com/wp-content/uploads/2022/06/one-piece-monkey-d-luffy-pfp-3.jpg",
+    "https://wallpapers-clan.com/wp-content/uploads/2022/06/one-piece-monkey-d-luffy-pfp-3.jpg"
+  ];
 
-  const name = ["Team Member", "Team Member", "Team Member", "Team Member"];
-  const description = ["A short description of the team member", "A short description of the team member", "A short description of the team member",  "A short description of the team member"];
-  const image = ["https://wallpapers-clan.com/wp-content/uploads/2022/06/one-piece-monkey-d-luffy-pfp-3.jpg", "https://wallpapers-clan.com/wp-content/uploads/2022/06/one-piece-monkey-d-luffy-pfp-3.jpg", "https://wallpapers-clan.com/wp-content/uploads/2022/06/one-piece-monkey-d-luffy-pfp-3.jpg", "https://wallpapers-clan.com/wp-content/uploads/2022/06/one-piece-monkey-d-luffy-pfp-3.jpg"];
-
-  return (
-    <div className="relative z-10 mb-20 flex w-[100vw] justify-center"> 
-      <div className="flex flex-wrap justify-center">
-        {name.map((name, index) => {
-          return (
-            <div key={index} className="w-full md:w-1/3 p-4 flex flex-col items-center">
-              <div className="">
-                <img src={image[index]} alt="Team Member" className="rounded-3xl" />
-              </div>
-              <div style={{ zIndex: 1 }} className="text-center">
-                <h3 className="text-2xl font-bold">{name}</h3>
-                <p>{description[index]}</p>
-              </div>
-            </div>
-          );
-        })}
-      </div>
-    </div>
-  );
+  return <TeamRoster names={names} descriptions={descriptions} images={images} />;
 }
