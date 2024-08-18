767ff590dda654e32a85284b4c36d57a7f863f67
avsomers25 <avsomers25@gmail.com>
2024-08-18 17:02:15 -0400

    s24 to f24

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,8 @@ import { Analytics } from "@vercel/analytics/react"
 
 export const metadata: Metadata = {
   title: {
-    template: '%s | HackRU S24',
-    default: 'HackRU S24',
+    template: '%s | HackRU F24',
+    default: 'HackRU F24',
   },
   description:
     "HackRU is a 24-hour hackathon hosted at Rutgers University. Join us for HackRU's Spring 2024 edition!",
@@ -18,7 +18,7 @@ export const metadata: Metadata = {
     'Hackathon, HackRU, Rutgers, Technology, Programming, Innovation, Challenge',
   //openGraph to set metadata for social media sharing
   openGraph: {
-    title: 'HackRU S24',
+    title: 'HackRU F24',
     description:
       'HackRU is a 24-hour hackathon Rutgers University. We welcome hundreds of students to join us in building awesome tech projects. Industry experts and mentors help foster an atmosphere of learning through tech-talks and one-on-one guidance. We encourage all students, no matter their experience level or educational background, to challenge themselves and expand their creative, technical, and collaboration skills at HackRU',
     images: '/icon.png',
