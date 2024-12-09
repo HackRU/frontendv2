5847eb7fe000fcc5b414b4e63a3feee78bdb326a
RyamL1221 <ryaml1221@gmail.com>
2024-12-09 13:10:20 -0500

    added team.ts

--- app/lib/api/team.ts
+++ app/lib/api/team.ts
@@ -2,24 +2,15 @@
 
 import { ENDPOINTS } from './endpoints';
 
-export async function UploadTeamSubmission(leaderEmail: string, data: any) {
-  const postBody = {
-    team_leader: leaderEmail,
-    team_members: data.team_members.filter(Boolean),
-  };
+export async function AttendEventScan(scannedEmail: string, event: string) {
+  const body = { scannedEmail, event };
 
-  const resp = await fetch(ENDPOINTS.makeTeam, {
+  const resp = await fetch(ENDPOINTS.attend, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(postBody),
+    body: JSON.stringify(body),
   });
 
-  if (resp.status !== 200) return { error: 'Non 200 response', response: '' };
-
-  const json = await resp.json();
-  if (json.error) {
-    return { error: json.error, response: '' };
-  }
-
-  return { error: '', response: 'Successfully submitted team!' };
+  const resJSON = await resp.json();
+  return resJSON;
 }
