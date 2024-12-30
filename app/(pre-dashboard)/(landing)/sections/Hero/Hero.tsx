a34a0f24bbd87b4343089e49b4604a4675a4f2ae
Kevin Monisit <monisitkevin@gmail.com>
2024-12-30 12:01:29 -0500

    Hero (#246)
    * Work on hero
    
    * Adjusting ramen bowl
    
    * Added image signs to hero buttons
    
    * Added textured background
    
    ---------
    
    Co-authored-by: Andrew Somers <67980093+avsomers25@users.noreply.github.com>

--- app/(pre-dashboard)/(landing)/sections/Hero/Hero.tsx
+++ app/(pre-dashboard)/(landing)/sections/Hero/Hero.tsx
@@ -6,8 +6,8 @@ import { useRouter } from 'next/navigation';
 import { getSelf } from '@/app/lib/data';
 import { bigelowRules, brush } from '@/app/ui/fonts';
 
-const FIRE_IMG = ['/landing/logo_f24_2.png'];
-const POKER_IMG = ['/landing/hackru_asset.png'];
+const FIRE_IMG = ['/landing/S2025/HACKRU_main_title.png'];
+const POKER_IMG = ['/landing/S2025/hero-main-art.png'];
 
 const animationTime = 800;
 const fireImageQuality = 10;
@@ -54,86 +54,115 @@ export default function Hero() {
     <>
       <Navbar />
       <div
-        className="relative flex w-full flex-col items-center justify-center overflow-hidden md:flex md:h-[100vh] md:flex-row-reverse"
-        id="Home"
+        className="w-full h-full flex justify-center items-center
+        bg-[url('/landing/S2025/texture-background.png')] bg-cover bg-center bg-no-repeat"
       >
-        <div className='relative flex w-full justify-center'>
-          <div className = 'h-auto w-[790px] xs:max-w-[400px] sm:max-w-[500px] md:w-[600px] md:min-w-[700px] lg:w-[800px] xl:w-[900px] z-10'>
-        <Image
-          src={POKER_IMG[0]}
-          alt="Poker"
-          width="900"
-          height="900"
-          className={'relative right-0 top-0'}
-          priority
-        />
-        </div>
-        </div>
-        <div className="relative flex flex-col items-center justify-center min-h-[400px] z-20">
-          <div
-            className="relative left-0 top-0 h-auto w-[790px]
-        xs:max-w-[320px] sm:max-w-[576px] md:w-[500px] md:min-w-[600px]
-        md:pl-8 lg:w-[700px] xl:w-[800px]"
-          >
-            <Image
-              src={FIRE_IMG[0]}
-              quality={fireImageQuality}
-              width="900"
-              height="900"
-              alt="Fire"
-              className={
-                "relative"
-              }
-              priority
-            />
+        <div
+          className="relative flex w-full flex-col items-center justify-center
+           sm:flex sm:h-[100vh] sm:flex-row-reverse max-w-[1100px]"
+          id="Home"
+        >
+          <div className='relative flex w-full justify-center'>
+            <div className='flex w-full items-end justify-end'>
+              <div className='h-auto w-[790px] xs:w-[400px] md:w-[400px] lg:w-[500px] relative'>
+                <div className="-mb-10 mt-20 md:-mb-0 md:mt-0 md:absolute md:w-[500px] -bottom-[200px] lg:w-[700px] lg:-bottom-[350px] right-[100px] md:right-[0px] md:-bottom-[200px] z-10">
+                  <Image
+                    src={POKER_IMG[0]}
+                    alt="Poker"
+                    width="2000"
+                    height="2000"
+                    priority
+                  />
+                  <Image
+                    src="/landing/S2025/smoke.png"
+                    alt="smoke"
+                    width="2000"
+                    height="2000"
+                    className="absolute -top-0 -left-32  sm:scale-[1.8] sm:-top-48 sm:-left-72 md:scale-[1.8]"
+                  />
+                </div>
+              </div>
+            </div>
+
           </div>
-          <div className="items-left flex-col  ">
-            <p
-              className={`${brush.className} text-2xl`}
+          <div className="relative flex flex-col items-center justify-center h-auto z-20 mb-40">
+            <div
+              className="relative left-0 top-0 h-auto w-[790px]
+            xs:max-w-[320px] sm:max-w-[576px] md:w-[500px]
+            md:pl-8 lg:w-[700px] xl:w-[800px] sm:-mt-52"
             >
-              <span className = "text-[#536F91]">THE GAMES ARE </span>
-              <span className = "text-[#ADD8E6]">ON!</span>
-            </p>
-            <p
-              className={`${brush.className} text-2xl`}
-            >
-              <span className = "text-[#ADD8E6]">Oct 26-27! </span>
-            </p>
-            <div className="mt-10 flex justify-center space-x-4">
-              <button
-                className="z-30 items-center justify-center
-            rounded-lg border-x-4 border-y-2 border-solid
-            border-[#C3557D] bg-transparent text-sm text-[#C3557D]
-            transition-all duration-100 hover:drop-shadow-[0_0_20px_#C3557D]
-            xs:h-[26px] xs:w-[99px]
-            sm:h-[45px] sm:w-[179px]
-            sm:text-lg md:h-[37px]
-            md:w-[145px] lg:h-[42px]
-            lg:w-[168px]"
-                onClick={() => router.push('/signup')}
+              <Image
+                src={FIRE_IMG[0]}
+                quality={fireImageQuality}
+                width="900"
+                height="900"
+                alt="Fire"
+                className={
+                  "relative"
+                }
+                priority
+              />
+            </div>
+            <div className="items-left flex-col">
+              <p
+                className={`${brush.className} text-2xl`}
               >
-                SIGN UP
-              </button>
-              <button
-                className="z-30 items-center justify-center
-            rounded-lg border-x-4 border-y-2 border-solid
-            border-[#EC9655] bg-transparent text-sm text-[#EC9655]
-            transition-all duration-100 hover:drop-shadow-[0_0px_20px_#EC9655]
-            xs:h-[26px] xs:w-[99px]
-            sm:h-[45px] sm:w-[179px]
-            sm:text-lg md:h-[37px]
-            md:w-[145px] lg:h-[42px]
-            lg:w-[168px]"
-                onClick={() => {
-                  if (isLogged) {
-                    router.push('/dashboard');
-                  } else {
-                    router.push('/login');
-                  }
-                }}
+                <span className="text-[#536F91]">it&apos;s time to cook! </span>
+                {/* <span className="text-[#ADD8E6]">ON!</span> */}
+              </p>
+              <p
+                className={`${brush.className} text-2xl`}
               >
-                {!isLogged ? 'LOG IN' : 'DASHBOARD'}
-              </button>
+                <span className="text-[#ADD8E6]">February 1st to 2nd! </span>
+              </p>
+              <div className="mt-10 flex justify-center space-x-4">
+                <button
+                  className="z-30 items-center justify-center relative
+                bg-transparent text-sm text-[#C3557D]
+                transition-all duration-100 hover:drop-shadow-[0_0_10px_#62a99d]
+                xs:h-[26px] xs:w-[99px]
+                sm:h-[45px] sm:w-[179px]
+                sm:text-lg md:h-[37px]
+                md:w-[145px] lg:h-[42px]
+                lg:w-[168px]"
+                  onClick={() => router.push('/signup')}
+                >
+                  <Image
+                    src="/landing/S2025/signup-button.png"
+                    alt="Sign up icon"
+                    width={400}
+                    height={400}
+                    className="absolute top-0 "
+                  />
+                  {/* SIGN UP */}
+                </button>
+                <button
+                  className="z-30 items-center justify-center relative
+                bg-transparent text-sm text-[#EC9655]
+                transition-all duration-100 hover:drop-shadow-[0_0px_20px_#EC9655]
+                xs:h-[26px] xs:w-[99px]
+                sm:h-[45px] sm:w-[179px]
+                sm:text-lg md:h-[37px]
+                md:w-[145px] lg:h-[42px]
+                lg:w-[168px]"
+                  onClick={() => {
+                    if (isLogged) {
+                      router.push('/dashboard');
+                    } else {
+                      router.push('/login');
+                    }
+                  }}
+                >
+                  <Image
+                    src="/landing/S2025/login-button.png"
+                    alt="Sign up icon"
+                    width={400}
+                    height={400}
+                    className="absolute top-0 "
+                  />
+                  <div className="">{!isLogged ? 'LOG IN' : 'DASHBOARD'}</div >
+                </button>
+              </div>
             </div>
           </div>
         </div>
