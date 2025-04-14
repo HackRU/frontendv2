8c4e61a515fd7badfa5ddf44d4d4df1aaada9856
Yogesh <yogeshsamkumar@gmail.com>
2025-04-14 12:37:59 -0400

    added layouts

---
+++ app/(pre-dashboard)/(entry)/signup/layout.tsx
@@ -0,0 +1,27 @@
+export default function Layout({ children
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+      <main className="w-fit h-fit "
+      style={{
+        '--bg-color': 'offblack-100',
+        '--bg-color2': 'offblack-100',
+        '--mainHeading-color': '#1E1E1E',
+        '--subHeading-color': '#4C855A',
+        '--InputBorder-color': '#4C855A',
+        '--InputPlaceholder-color': '#4C855A',
+        '--error-color': '#4C855A',
+        '--redirect-color': '#4C855A',
+        '--hover-color': '#4C855A',
+      } as React.CSSProperties}>
+        {children}
+        <div
+          id="entry-radial"
+          className="top-0 w-full h-full absolute -z-10"
+        />
+      </main>
+    </>
+  )
+}
