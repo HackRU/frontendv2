793934928bdc7e33a0ee2f3c76979aafc02f3894
RyamL1221 <ryaml1221@gmail.com>
2025-05-15 13:08:11 -0400

    created delete function

--- app/dashboard/views/directorView.tsx
+++ app/dashboard/views/directorView.tsx
@@ -28,8 +28,7 @@ function DirectorView(userData: any) {
   const [currentPage, setPage] = useState<number>(1);
   const [totalPages, setTotal] = useState<number>(1);
 
-  useEffect(() => {
-    async function fetchUsers() {
+  const fetchUsers = async () => {
       try {
         const data = await GetAllUsers();
         const users = data.response;
@@ -44,6 +43,16 @@ function DirectorView(userData: any) {
       }
     }
 
+  const handleDelete = async (email: string) => {
+    try { 
+      await DeleteUser(email);
+      await fetchUsers();
+    } catch(error) {
+      console.log(error);
+    }
+      
+  }
+  useEffect(() => {
     fetchUsers();
   }, []);
   
@@ -260,7 +269,7 @@ function DirectorView(userData: any) {
                 <button className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-accent hover:text-accent-foreground h-10 px-4 py-2 mr-2">
                   Cancel
                 </button>
-                <button onClick={() => DeleteUser(users[email].email)} className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 text-primary-foreground hover:bg-primary/90 h-10 px-4 py-2 bg-blue-600 border-blue-600">
+                <button onClick={() => handleDelete(users[email].email)} className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 text-primary-foreground hover:bg-primary/90 h-10 px-4 py-2 bg-blue-600 border-blue-600">
                   Delete
                 </button>
               </form>
