f98973af74ae349fed0696258764d04921083ac6
Andrew Somers <67980093+avsomers25@users.noreply.github.com>
2025-08-25 15:31:12 -0400

    small fixes

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,12 +1,13 @@
 import { Suspense } from 'react';
 import Cursor from '../ui/cursor';
 import Navbar from '../(pre-dashboard)/(landing)/sections/Hero/Navbar';
+import { inter } from '@/app/ui/fonts';
 //import { StarryBackground } from "../(pre-dashboard)/(landing)/misc/StarsBackground";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <main className="relative h-screen w-screen" id="entry-radial">
+      <main className={`relative h-screen w-screen ${inter.className}`} id="entry-radial">
         <Navbar />
         <Suspense>
           <Cursor />
